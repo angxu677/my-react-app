31662198f58b27f908c14fbee5601f2117be7d8c
Angela Xu <angela.xu@wustl.edu>
2025-11-09 17:26:33 -0600

    updates board component to render square component

--- src/App.jsx
+++ src/App.jsx
@@ -23,3 +23,25 @@ export default function Board() {
 function Square() {
   return <button className="square">1</button>;
 }
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
